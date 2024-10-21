b2e714003d38990e345ed26b40ca3e0e21ede5ce
MewMew <charleeeeswangj@gmail.com>
2024-10-21 11:03:15 +0900

    article uploading viewing and liking

--- backend/router/router.go
+++ backend/router/router.go
@@ -22,17 +22,21 @@ func SetupRouter() *gin.Engine {
 	})
 
 	auth := r.Group("/api/auth")
-	{
-		auth.POST("/register", controllers.Register)
-		auth.POST("/login", controllers.Login)
-	}
+	//
+	auth.POST("/register", controllers.Register)
+	auth.POST("/login", controllers.Login)
+	//
 
 	api := r.Group("/api")
 	api.GET("/exchangeRates", controllers.GetExchangeRate)
 	api.Use(middlewares.AuthMiddleware())
-	{
-		api.POST("/exchangeRates", controllers.CreateExchangeRate)
-	}
-
+	//
+	api.POST("/exchangeRates", controllers.CreateExchangeRate)
+	api.POST("/articles", controllers.CreateArticle)
+	api.GET("/articles", controllers.GetArticles)
+	api.GET("/articles/:id", controllers.GetArticleByID)
+	//
+	api.POST("/articles/:id/like", controllers.LikeArticle)
+	api.GET("/articles/:id/likes", controllers.GetArticleLikes)
 	return r
 }
