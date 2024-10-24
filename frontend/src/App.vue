526a52080ee2ec60160bc31c4cad3ce9b47c2c4a
MewMew <charleeeeswangj@gmail.com>
2024-10-24 13:19:25 +0900

    menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>Main</el-main>
-    </el-container>
-  </div>
+  <el-container>
+      <el-header>
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          :ellipsis="false"
+          @select="handleSelect"
+        >
+          <el-menu-item index="home">Home</el-menu-item>
+          <el-menu-item index="currencyExchange">CurrencyExchange</el-menu-item>
+          <el-menu-item index="news">News</el-menu-item>
+          <el-menu-item index="login">Login</el-menu-item>
+          <el-menu-item index="register">Register</el-menu-item>
+          <el-menu-item index="logout">Logout</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+      </el-main>
+  </el-container>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('home')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
